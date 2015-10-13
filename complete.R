6235c3155f29e8076d26fbba5dc4632ecdb7af4e
pmtempone <pmtempone@gmail.com>
2015-10-13 00:59:34 -0300

    complete resuelto
    ejercicio 2, semana 2, curso R programming, resuelto

--- complete.R
+++ complete.R
@@ -1,9 +1,21 @@
 complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
-  
+  file_list <- list.files(directory, full.names=TRUE)
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
+  complete_data <- data.frame()
+  
+  final_data <- data.frame()
+  
+  for (i in id) {                                
+    complete_data <- read.csv(file_list[i])
+    count_complete_data <- data.frame(id= i,nobs = sum(complete.cases(complete_data)))
+    final_data <- rbind(final_data,count_complete_data)
+  }
+  
+  final_data
+  ##unique(data.frame(id=complete_data$ID,nobs=sum(complete.cases(complete_data))))
   
   ## Return a data frame of the form:
   ## id nobs
